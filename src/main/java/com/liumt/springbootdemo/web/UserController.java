d3cfed14903caa36037eb87f7cb1ed4c494a7027
kadlmt <943747060@qq.com>
2020-10-27 07:03:44 +0800

    1

--- src/main/java/com/liumt/springbootdemo/web/UserController.java
+++ src/main/java/com/liumt/springbootdemo/web/UserController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/v1.0/user")
 public class UserController {
 
 	/**
